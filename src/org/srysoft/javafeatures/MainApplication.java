96a67476a999363f55389dcfd408dc8c33417dcb
Satyanarayan Yadav <satyanarayan.y@gmail.com>
2019-02-24 00:22:40 +0530

    Number Array List

--- src/org/srysoft/javafeatures/MainApplication.java
+++ src/org/srysoft/javafeatures/MainApplication.java
@@ -1,28 +1,30 @@
 package org.srysoft.javafeatures;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication {
 
 	public static void main(String[] args) {
-		
-		List<String> list1 = new ArrayList<String>();
-		list1.add("Apple");
-		list1.add("Bat");
-		list1.add("Cat");
-		list1.add("Dog");
-		list1.add("Elephant");
-		list1.add("Fish");
-		list1.add("Goat");
-		
-		Program1 prog1 = new Program1();
-		//prog1.printWithFP(list1);
-		prog1.printWithFilterFP(list1);
-		
-		
-		
-		
+
+		String[] list1 = { "Apple", "Bat", "Cat", "Dog", "Elephant", "Fist", "Goat" };
+		List<String> alphabet = Arrays.asList(list1);
+
+		//Program1 prog1 = new Program1();
+		//prog1.printWithFP(alphabet);
+		//prog1.printWithFilterFP(alphabet);
+
+		Integer[] list2 = { 4, 6, 9, 8, 13, 11, 15, 4, 9, 8, 3};
+		List<Integer> numbers = Arrays.asList(list2);
+		Program2 prog2 = new Program2();
+		//prog2.printNumbers(numbers);
+		//prog2.sumNumbers(numbers);
+		//prog2.sumOddNumbers(numbers);
+		//prog2.sortNumbers(numbers);
+		prog2.showDistinctNumber(numbers);
+		System.out.println("---------------------------------");
+		prog2.showDistinctSquareNumber(numbers);
+		System.out.println("---------------------------------");
 
 	}
 
---
+++ src/org/srysoft/javafeatures/Program2.java
@@ -0,0 +1,36 @@
+package org.srysoft.javafeatures;
+
+import java.util.List;
+
+public class Program2 {
+
+	public void printNumbers(List<Integer> numbers) {
+
+		numbers.stream().forEach(element -> System.out.println(element));
+	}
+
+	public void sumNumbers(List<Integer> numbers) {
+
+		int sum = numbers.stream().reduce(0, (num1, num2) -> num1 + num2);
+		System.out.println("Sum = " + sum);
+	}
+
+	public void sumOddNumbers(List<Integer> numbers) {
+		int sum = numbers.stream().filter(num -> num % 2 == 1).reduce(0, (num1, num2) -> num1 + num2);
+		System.out.println("Sum of Odd Number = " + sum);
+	}
+
+	public void sortNumbers(List<Integer> numbers) {
+		numbers.stream().sorted().forEach(num -> System.out.println(num));
+
+	}
+
+	public void showDistinctNumber(List<Integer> numbers) {
+		numbers.stream().sorted().distinct().forEach(num -> System.out.println(num));
+	}
+
+	public void showDistinctSquareNumber(List<Integer> numbers) {
+		numbers.stream().sorted().distinct().map(num -> num * num).forEach(num -> System.out.println(num));
+	}
+
+}
